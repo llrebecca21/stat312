219ca0d17f86d3b6690af937aa8a0298da3e8154
llrebecca21 <llrebecca21@gmail.com>
2024-09-15 19:10:07 -0500

    Update the Description file and create an Imports file for the required packages for this course.

---
+++ R/Imports.R
@@ -0,0 +1,11 @@
+#' @import stats
+#' @import MASS
+#' @import vcd
+#' @import HardyWeinberg
+#' @import ggplot2
+#' @import lme4
+#' @import Matrix
+#' @import mixtools
+#' @import vegan
+#' @import ape
+NULL
